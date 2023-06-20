3a22350bf91074c2ada8532675093ddcc9e31040
Lucas Machado de Souza <lcsmachaado@gmail.com>
2023-06-20 13:51:41 -0300

    rename SpringbootApplication.java

--- src/main/java/dev/lucasmachado/SpringbootApplication.java
+++ src/main/java/dev/lucasmachado/SpringbootApplication.java
@@ -5,19 +5,17 @@ import dev.lucasmachado.repositories.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
 @SpringBootApplication
-public class AttornatusApplication implements CommandLineRunner {
+public class SpringbootApplication implements CommandLineRunner {
 
     @Autowired
     private PessoaRepository pessoaRepository;
 
     public static void main(String[] args) {
-        SpringApplication.run(AttornatusApplication.class, args);
+        SpringApplication.run(SpringbootApplication.class, args);
     }
     @Override
     public void run(String... args) throws Exception {
