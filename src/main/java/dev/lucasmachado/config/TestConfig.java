0c6d1cea6b320cddea2a93282d2253a9e2cef2a7
Lucas Machado de Souza <lcsmachaado@gmail.com>
2023-06-22 14:53:01 -0300

    Updated TestConfig for auto-generate some data

--- src/main/java/dev/lucasmachado/config/TestConfig.java
+++ src/main/java/dev/lucasmachado/config/TestConfig.java
@@ -1,7 +1,9 @@
 package dev.lucasmachado.config;
 
+import dev.lucasmachado.services.DbService;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.Consumer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -11,41 +13,52 @@ import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 
+import java.text.ParseException;
+
 @Configuration
 @Profile("test")
 public class TestConfig {
 
-    @Bean
-    public NewTopic topic() {
-        return new NewTopic("example-topic", 10, (short) 0);
-    }
-
-    @KafkaListener(topicPartitions = @TopicPartition(topic = "example-topic",
-            partitions = "#{@finder.partitions('example-topic')}"))
-    public void listen(@Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) String key, String payload) {
-        System.out.println(key + ":" + payload);
-    }
+    @Autowired
+    private DbService dbService;
 
     @Bean
-    public PartitionFinder finder(ConsumerFactory<String, String> consumerFactory) {
-        return new PartitionFinder(consumerFactory);
+    public boolean instantiateDatabase() throws ParseException {
+        dbService.instantiateTestDatabase();
+        return true;
     }
 
-    public static class PartitionFinder {
-
-        public PartitionFinder(ConsumerFactory<String, String> consumerFactory) {
-            this.consumerFactory = consumerFactory;
-        }
-
-        private final ConsumerFactory<String, String> consumerFactory;
-
-        public String[] partitions(String topic) {
-            try (Consumer<String, String> consumer = consumerFactory.createConsumer()) {
-                return consumer.partitionsFor(topic).stream()
-                        .map(pi -> "" + pi.partition())
-                        .toArray(String[]::new);
-            }
-        }
-    }
+//    @Bean
+//    public NewTopic topic() {
+//        return new NewTopic("example-topic", 10, (short) 0);
+//    }
+//
+//    @KafkaListener(topicPartitions = @TopicPartition(topic = "example-topic",
+//            partitions = "#{@finder.partitions('example-topic')}"))
+//    public void listen(@Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) String key, String payload) {
+//        System.out.println(key + ":" + payload);
+//    }
+//
+//    @Bean
+//    public PartitionFinder finder(ConsumerFactory<String, String> consumerFactory) {
+//        return new PartitionFinder(consumerFactory);
+//    }
+//
+//    public static class PartitionFinder {
+//
+//        public PartitionFinder(ConsumerFactory<String, String> consumerFactory) {
+//            this.consumerFactory = consumerFactory;
+//        }
+//
+//        private final ConsumerFactory<String, String> consumerFactory;
+//
+//        public String[] partitions(String topic) {
+//            try (Consumer<String, String> consumer = consumerFactory.createConsumer()) {
+//                return consumer.partitionsFor(topic).stream()
+//                        .map(pi -> "" + pi.partition())
+//                        .toArray(String[]::new);
+//            }
+//        }
+//    }
 
 }
