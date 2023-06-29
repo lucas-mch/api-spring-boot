e188a1e776af030d7ae96d842f270281e7222edc
Lucas Machado de Souza <lcsmachaado@gmail.com>
2023-06-29 14:15:50 -0300

    #DTO bean validation and exception handler for validation errors

--- src/main/java/dev/lucasmachado/enterprise/exceptions/ResourceExceptionHandler.java
+++ src/main/java/dev/lucasmachado/enterprise/exceptions/ResourceExceptionHandler.java
@@ -3,6 +3,8 @@ package dev.lucasmachado.enterprise.exceptions;
 import org.hibernate.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -22,5 +24,17 @@ public class ResourceExceptionHandler {
         StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> dataIntegrity(MethodArgumentNotValidException e, HttpServletRequest request) {
+        ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(),"Validation error!", System.currentTimeMillis());
+
+        for(FieldError x : e.getBindingResult().getFieldErrors()){
+            err.addError(new FieldMessage(x.getField(),x.getDefaultMessage()));
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
+
 
 }
