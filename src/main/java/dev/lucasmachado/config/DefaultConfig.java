f7361c982e3ad96f249801dd6853544d4824ce02
Lucas Machado <lcsmachaado@gmail.com>
2023-07-04 15:58:28 -0300

    #fix pipe on github

--- src/main/java/dev/lucasmachado/config/DefaultConfig.java
+++ src/main/java/dev/lucasmachado/config/DefaultConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("default")
+@Profile("prod")
 public class DefaultConfig {
 
     @Bean
